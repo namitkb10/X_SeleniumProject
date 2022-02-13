b5143159bb344203c4fde364e2a37fb28855aa78
namitkb10 <namitkb10@gmail.com>
2022-02-13 16:11:59 +0530

    Added Integrate with GitHub: build after each commit (Get started with Jenkins, part 13)

---
+++ src/test/java/execute_In_Sequence/In_Sequence.java
@@ -0,0 +1,14 @@
+package execute_In_Sequence;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class In_Sequence {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Executed Testcases in Sequence");
+		Assert.assertTrue(true);
+	}
+}
